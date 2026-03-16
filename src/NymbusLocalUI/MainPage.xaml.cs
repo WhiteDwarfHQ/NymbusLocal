1782e3966873c67f6d1114f42009e3c7998f8243
Chris Baker <mr.baker025@gmail.com>
2026-03-15 23:58:19 -0400

    Initial Repository Structure:
    - .NET MAUI Blazor app setup for NymbusLocalUI

---
+++ src/NymbusLocalUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NymbusLocalUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/NymbusLocalUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NymbusLocalUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
